eafa30937954199195ce8d4f327e8333b68095d4
Adnin03 <adnindanil03@gmail.com>
2026-02-01 07:33:53 +0700

    Update visualisasi pertanyaan ke dua dan menambah interaksi di dalam dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -24,7 +24,49 @@ main_df['datetime'] = pd.to_datetime(main_df['datetime'])
 # Menyiapkan DataFrame
 monthly_trend_df = create_monthly_trend_df(main_df)
 
+# Filter Waktu
+with st.sidebar:
+    st.markdown("<h1 style='text-align: center;'>Air Quality Analysis</h1>", unsafe_allow_html=True)
+    img_path = os.path.join(current_dir, 'awan.png')
+    st.image(img_path)
+    
+    min_date = main_df['datetime'].min()
+    max_date = main_df['datetime'].max()
+    
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+    
+main_df_fitered = main_df[(
+    (main_df['datetime'] >= str(start_date)) &
+    (main_df['datetime'] <= str(end_date))
+)]
+
+# Filter Stasiun 
+with st.sidebar:
+    selected_stations = st.multiselect(
+        label='Pilih Stasiun',
+        options=main_df['station'].unique(),
+        default=[]
+    )
+    
+main_df_fitered = main_df_fitered[main_df_fitered['station'].isin(selected_stations)]
+
 st.header('Air Quality Analysis Dashboard ☁️')
+
+st.subheader('Informasi Ringkas Kualitas Udara')
+
+col1, col2 = st.columns(2)
+with col1:
+    avg_pm25 = main_df['PM2.5'].mean()
+    st.metric("Rata-rata PM2.5", value=f"{avg_pm25:.2f} µg/m³")
+
+with col2:
+    max_temp = main_df['TEMP'].max()
+    st.metric("Suhu Maksimum", value=f"{max_temp:.1f} °C")
 
 # Menampilkan pertanyaan pertama
 st.subheader('Tren Rata-rata PM2.5 Seluruh Stasiun (2013-2017)')
@@ -47,23 +89,33 @@ with st.expander('Lihat Insight Tren'):
     )
     
 # Menampilkan pertanyaan kedua
-st.subheader('Korelasi antara Suhu (TEMP) dan Kadar Ozon (O3)')
+st.subheader('Korelasi antara Suhu (TEMP) dan Konsentrasi PM2.5 di Aotizhongxin')
+df_aoti = main_df[main_df['station'] == 'Aotizhongxin']
+correlation = df_aoti['TEMP'].corr(df_aoti['PM2.5'])
 
 fig, ax = plt.subplots(figsize=(10, 6))
 sns.regplot(
-    x=main_df['TEMP'],
-    y=main_df['O3'],
-    scatter_kws={'alpha':0.1, 'color':'#3498db'},
-    line_kws={'color':'red'},
+    data=df_aoti,
+    x='TEMP',
+    y='PM2.5',
+    scatter_kws={'alpha': 0.3, 'color': 'skyblue'},
+    line_kws={'color': 'red'},
     ax=ax
 )
-ax.set_xlabel('Suhu (Celcius)', fontsize=12)
-ax.set_ylabel('Kadar Ozon (O3)', fontsize=12)
-st.pyplot(fig)
 
+ax.set_title(f'Koefisien Korelasi: {correlation:.2f}', fontsize=14)
+ax.set_xlabel('Suhu (Celsius)', fontsize=12)
+ax.set_ylabel('Konsentrasi PM2.5 (µg/m³)', fontsize=12)
+ax.grid(True, linestyle='--', alpha=0.5)
+
+st.pyplot(fig)
 with st.expander('Lihat Insight Korelasi'):
     st.write(
-        """Terdapat korelasi positif yang cukup kuat (0.61) antara suhu udara dan kadar Ozon. Semakin tinggi suhu udara, maka pembentukan Ozon cenderung semakin meningkat"""
+        f"""
+        Berdasarkan grafik di atas, ditemukan korelasi **negatif yang lemah ({correlation:.2f})** antara suhu udara dan konsentrasi PM2.5 di stasiun Aotizhongxin. 
+        
+        Hal ini menunjukkan bahwa peningkatan suhu cenderung diikuti oleh sedikit penurunan kadar PM2.5. Hal ini kemungkinan disebabkan oleh kondisi atmosfer yang lebih tidak stabil saat suhu panas, sehingga memudahkan penyebaran polutan. Namun, karena nilainya mendekati nol, suhu bukanlah faktor tunggal utama yang menentukan kualitas udara di lokasi ini.
+        """
     )
     
 st.caption('Copyright (c) Dicoding 2026')
